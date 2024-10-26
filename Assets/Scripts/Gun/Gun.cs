a5da29556eedae49ce7ebe9d0295cbf341026ace
Nikita <kurnosov.n.a.15@gmail.com>
2024-10-26 16:25:21 +0300

    Gun rotating added

---
+++ Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private float _offset;
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + _offset);
+    }
+}
